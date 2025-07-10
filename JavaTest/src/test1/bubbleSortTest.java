8c3346f64680b0a96be3414b167015ee0cd3f44d
JinIleSon <166898497+JinIleSon@users.noreply.github.com>
2025-07-10 10:30:22 +0900

    Add files via upload

--- JavaTest/src/test1/bubbleSortTest.java
+++ JavaTest/src/test1/bubbleSortTest.java
@@ -2,4 +2,25 @@ package test1;
 
 public class bubbleSortTest {
 
+	public static void bubbleSort(int[] arr) {
+		
+		int a = arr.length;
+		
+		for(int i = 0; i < n - 1; i++) {
+			
+			for(int j = 0; j < n - 1 - i; j++) {
+				
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] data = {};
+	}
 }
